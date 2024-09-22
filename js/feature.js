08ce90cf0f42c67941709e5e684706191a995f40
mohammaduzzal <aalp72142@gmail.com>
2024-09-22 23:54:46 +0600

    added the header section

--- js/feature.js
+++
@@ -1,1 +0,0 @@
-console.log('matal joy');
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: 'rgba(180, 244, 97, 1)',
+          secondary:'rgba(249, 247, 243, 1)'
+        },
+      },
+    },
+  };
